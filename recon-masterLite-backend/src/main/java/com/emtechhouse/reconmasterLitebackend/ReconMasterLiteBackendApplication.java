66a0c8160e25236ad8ea0dcb659fd51188ea1340
Stiflerzak <hilarymwendia97@gmail.com>
2023-09-20 12:34:43 +0300

    initial backend

---
+++ recon-masterLite-backend/src/main/java/com/emtechhouse/reconmasterLitebackend/ReconMasterLiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emtechhouse.reconmasterLitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconMasterLiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconMasterLiteBackendApplication.class, args);
+	}
+
+}
